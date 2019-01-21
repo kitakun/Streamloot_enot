35b9341bfcdf9e14eb488d693eae90b8d93dbe3f
Kitakun <xfikysx@ya.ru>
2019-01-21 21:11:40 +0700

    interceptor adjust

--- src/app/Interceptors/SHttpInterceptor.ts
+++ src/app/Interceptors/SHttpInterceptor.ts
@@ -5,14 +5,6 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class SHttpInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
-
-        let head = new HttpHeaders();
-        head.append('Access-Control-Allow-Headers', 'Content-Type');
-        head.append('Access-Control-Allow-Methods', 'GET');
-        head.append('Access-Control-Allow-Origin', '*');
-
-        head.set('sl-shop-id', '1');
-
         const paramReq = req.clone({
             setHeaders: {
                 'sl-shop-id': `1`
